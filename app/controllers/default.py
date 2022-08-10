ea001635ee029ea092aff29f9906d578303b4434
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-08-10 19:04:24 -0300

    Add listagem route and method.

---
+++ app/controllers/default.py
@@ -0,0 +1,11 @@
+from flask import render_template, request
+from app import app, db
+from app.models.tables import Pessoa
+
+#As duas rotas apontam para o mesmo local (duas rotas seguidas)
+@app.route('/')
+@app.route('/listagem')
+def listagem():
+	pessoas = Pessoa.query.all()
+	return render_template('listagem.html', pessoas=pessoas, ordem='id')
+
